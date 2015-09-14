81cfb7f57afca3ae0d3c02fcbc9cdea639dedcf6
علي <alivideogamer@hotmail.com>
2015-09-14 21:55:02 +0300

    1st  joomla git

---
+++ se415psu151/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-09-14 13:32:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-09-14T13:32:10+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-09-14T13:32:49+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
